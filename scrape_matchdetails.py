3f9fc0a3c60a9a6f60881725716b81c3766d33eb
uscmuenster <wl@pascal365.de>
2025-07-27 19:41:19 +0200

    Aktualisieren von scrape_matchdetails.py

--- scrape_matchdetails.py
+++ scrape_matchdetails.py
@@ -6,7 +6,10 @@ from datetime import datetime
 url = "https://www.volleyball-bundesliga.de/popup/matchSeries/matchDetails.xhtml?matchId=771119420"
 
 # HTML laden
-response = requests.get(url)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+}
+response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 
 # Extrahiere alle relevanten Textinformationen
