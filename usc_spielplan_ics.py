2a10f958def5533cfceaa4cf8c7dfed03504302a
uscmuenster <wl@pascal365.de>
2025-07-31 08:58:22 +0200

    Aktualisieren von usc_spielplan_ics.py

--- usc_spielplan_ics.py
+++ usc_spielplan_ics.py
@@ -10,6 +10,15 @@ csv_files = [
     ("Spielplan_1._Bundesliga_Frauen.csv", "USC1"),
     ("Spielplan_2._Bundesliga_Frauen_Nord.csv", "USC2"),
     ("Spielplan_Oberliga_2_Frauen.csv", "USC3"),
+    ("Spielplan_Bezirksliga_14_Frauen.csv", "USC4"),
+    ("Spielplan_NRW-Liga_wU14.csv", "USC-U14-1"),
+    ("Spielplan_NRW-Liga_wU16.csv", "USC-U16-1"),
+    ("Spielplan_NRW-Liga_wU18.csv", "USC-U18"),
+    ("Spielplan_Oberliga_5_wU14.csv", "USC-U14-2"),
+    ("Spielplan_Oberliga_5_wU16.csv", "USC-U16-2"),
+    ("Spielplan_Oberliga_6_wU13.csv", "USC-U13"),
+    ("Spielplan_Bezirksklasse_26_Frauen.csv", None),
+    ("Spielplan_Kreisliga_Muenster_Frauen.csv", None),
 ]
 
 usc_keywords = ["USC Münster", "USC Muenster", "USC MÜNSTER"]
